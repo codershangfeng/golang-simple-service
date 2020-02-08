2a72499ae14f6d1c60db9b678a270514220fdb71
codershangfeng <codershangfeng@gmail.com>
2020-02-08 10:19:32 +0800

    Add authorization service in incoming port layer.

---
+++ src/incoming-port/AuthorizationService.go
@@ -0,0 +1,29 @@
+package incomingport
+
+import "time"
+
+type AuthorizationService interface {
+	Authorize(IncomingAuthorizationRequest) IncomingAuthorizationResponse
+}
+
+type IncomingAuthorizationRequest struct {
+	UUID              string
+	AuthorizationCode string
+}
+
+type IncomingAuthorizationResponse struct {
+	Result       ResultCode
+	ErrorMessage string
+	CreatedTime  time.Time
+}
+
+type ResultCode int
+
+const (
+	Approved ResultCode = iota
+	Error
+)
+
+func (rc ResultCode) String() string {
+	return [...]string{"APPROVED", "ERROR"}[rc]
+}
